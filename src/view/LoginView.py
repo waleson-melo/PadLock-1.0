f9051f341cbc27e63f57c31abe54a38eddc5d148
Waleson Melo <walesonmelo23@gmail.com>
2021-09-06 14:15:10 -0400

    template da janela de login criado

--- src/view/LoginView.py
+++ src/view/LoginView.py
@@ -4,7 +4,25 @@ import PySimpleGUI as sg
 class LoginView:
 
     def __init__(self):
-        pass
+        user_image = "icons/user.png" 
+
+        col1 = [
+            [sg.Text(text='Nome:')], 
+            [sg.Input(size=(21,1))],
+            [sg.Text(text='Senha:')],
+            [sg.Input(size=(21,1), password_char='*')],
+            [sg.Button(button_text='Entrar', size=(6,1)), sg.Button(button_text='Sair', size=(6,1))]
+        ]
+
+        frame_image = [
+            [sg.Image(user_image)]
+        ]
+
+        layout = [
+            [sg.Frame(title='', layout=frame_image), sg.Col(col1)],
+        ]
+
+        self.login_window = sg.Window("Entrar no sistema", layout=layout, finalize=True)
 
     def start(self):
-        pass
+        return self.login_window
