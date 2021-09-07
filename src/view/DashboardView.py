6bef65747b500c71961b46a48b93f63ccdd3fb65
Waleson Melo <walesonmelo23@gmail.com>
2021-09-07 16:18:53 -0400

    Update DashboardView.py
    Estrutura inicial do arquivo

---
+++ src/view/DashboardView.py
@@ -0,0 +1,16 @@
+import PySimpleGUI as sg
+
+
+class DashboardView:
+
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+
+if __name__ == '__main__':
+    dash = DashboardView()
+
+    dash.start()
