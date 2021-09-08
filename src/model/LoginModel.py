b6b6d89f1201322cb1429fa2909cf30e737e7708
Waleson Melo <walesonmelo23@gmail.com>
2021-09-08 16:42:57 -0400

    Adcionada a funcionalidade de validar o usuario

--- src/model/LoginModel.py
+++ src/model/LoginModel.py
@@ -1,8 +1,9 @@
 import src.util.ConnectionDB as conn
 
-class LoginModel:
+class LoginModel(conn.ConnectionDB):
 
     def __init__(self, login: str, senha: str):
+        super().__init__()
         self.login = login
         self.senha = senha
 
@@ -28,6 +29,20 @@ class LoginModel:
         
         self.__senha = value.strip()
 
+    def validar_usuario(self):
+        self.connect_database()
+
+        sql = """
+            SELECT * FROM usuario WHERE nome = ? AND senha = ?
+        """
+        dados = self.conn.execute(sql, (self.login, self.senha)).fetchone()
+
+        self.desconnect_db()
+
+        if dados is not None:
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
     log = LoginModel(login='None', senha='admin')
