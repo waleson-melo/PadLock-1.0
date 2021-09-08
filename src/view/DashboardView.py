e139d79908954ebd3a213895f857771f35e319fa
Waleson Melo <walesonmelo23@gmail.com>
2021-09-07 20:22:59 -0400

    Update DashboardView.py
    estilo da janela com os campus usuario

--- src/view/DashboardView.py
+++ src/view/DashboardView.py
@@ -4,10 +4,64 @@ import PySimpleGUI as sg
 class DashboardView:
 
     def __init__(self):
-        pass
+        menu_bar = [
+            ['&Gerenciar', ['&Senhas', '&Usuário']],
+            ['&Ajuda', 'So&bre']
+        ]
+
+        col_senhas = [
+            [sg.Input('', size=(21,1), key='-IN_PESQUISA-'), sg.Button('Atualizar')]
+        ]
+
+        size_text = (12,1)
+        col_usuario = [
+            [sg.Text('Código:', size=size_text), sg.Input('', expand_y=True,
+                key='-IN_CODIGO_USUARIO-', disabled=True)],
+            [sg.Text('Nome:', size=size_text), sg.Input('', expand_y=True,
+                key='-IN_NOME_USUARIO-')],
+            [sg.Text('Senha:', size=size_text), sg.Input('', expand_y=True,
+                key='-IN_SENHA_USUARIO-')],
+            [sg.Text('Repetir senha:', size=size_text),
+                sg.Input('', expand_y=True, key='-IN_REPETIR_SENHA_USUARIO-')],
+            [sg.Checkbox('Marque aqui para editar.', default=False,
+                enable_events=True, key='-CHECKBOX_EDITAR-')],
+            [sg.Button('Salvar Usuário')]
+        ]
+
+        layout = [
+            [sg.Text('Senhas', font='Ubuntu 15 normal', key='-TEXT_OPCAO-')],
+            [sg.Menu(menu_bar, tearoff=False)],
+            [sg.Column(col_senhas, key='-COL_SENHAS-'), sg.Canvas(size=(0,0), pad=(0,0))],
+            [sg.Column(col_usuario, key='-COL_USUARIO-'), sg.Canvas(size=(0,0), pad=(0,0))]
+        ]
+
+        self.window = sg.Window('Gerenciador de Senhas', layout=layout, 
+                                finalize=True, size=(300,250), margins=(3,3))
+
+        self.window['-COL_USUARIO-'].update(visible=False)
+
 
     def start(self):
-        pass
+        while True:
+            event, values = self.window.read()
+
+            if event == sg.WINDOW_CLOSED or event == 'Sair':
+                break
+
+            if event == 'Usuário':
+                self.window['-TEXT_OPCAO-'].update('Usuário')
+                self.window['-IN_NOME_USUARIO-'].set_focus()
+                self.window['-COL_SENHAS-'].update(visible=False)
+                self.window['-COL_USUARIO-'].update(visible=True)
+            
+            if event == 'Senhas':
+                self.window['-TEXT_OPCAO-'].update('Senhas')
+                self.window['-IN_PESQUISA-'].set_focus()
+                self.window['-COL_USUARIO-'].update(visible=False)
+                self.window['-COL_SENHAS-'].update(visible=True)
+                
+        
+        self.window.close()
 
 
 if __name__ == '__main__':
