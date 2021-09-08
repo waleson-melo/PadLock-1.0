ba8e87ad6e6ee21e24e817c77a126abd73f3ed99
Waleson Melo <walesonmelo23@gmail.com>
2021-09-08 13:10:35 -0400

    Layout de Nova senha criado

--- src/view/SenhaView.py
+++ src/view/SenhaView.py
@@ -6,13 +6,23 @@ class SenhaView:
     def __init__(self, opcao='NOVO'):
         if opcao == 'NOVO':
             self.title_window = 'Nova Senha'
+            size_text = (18,1)
+            size_input = (30,1)
             self.layout = [
-                [sg.Text('Título:')],
-                [sg.Text('Login:')],
-                [sg.Text('Senha:')],
-                [sg.Text('URL do login:')],
-                [sg.Text('Data de modificação:')],
-                [sg.Text('Observações:')]
+                [sg.Text('Título:', size=size_text), sg.Input('', size=(size_input),
+                    key='-IN_TITULO_SENHA-')],
+                [sg.Text('Login:', size=size_text), sg.Input('', size=(size_input),
+                    key='-IN_LOGIN_SENHA-')],
+                [sg.Text('Senha:', size=size_text), sg.Input('', size=(size_input),
+                    key='-IN_SENHA_SENHA-')],
+                [sg.Text('URL do login:', size=size_text), sg.Input('', size=(size_input),
+                    key='-IN_URL_SENHA-')],
+                [sg.Text('Data de modificação:', size=size_text), sg.Input('', size=(size_input),
+                    key='-IN_DATAMODIFICACAO_SENHA-')],
+                [sg.Text('Observações:', size=size_text), sg.Input('', size=(size_input),
+                    key='-IN_OBSERVACOES_SENHA-')],
+                [sg.Button('Salvar'), sg.Button('Limpar'),
+                    sg.Button('Cancelar')]
             ]
         elif opcao == 'ALTERAR':
             self.title_window = 'Alterar Senha'
@@ -26,7 +36,7 @@ class SenhaView:
             ]
 
         self.window = sg.Window(self.title_window, layout=self.layout,
-                                finalize=True)
+                                finalize=False)
 
     def start(self):
         while True:
@@ -34,11 +44,16 @@ class SenhaView:
             
             if event == sg.WINDOW_CLOSED:
                 break
+            
+            if event == 'Cancelar':
+                if sg.popup_yes_no('Você deseja realmente cancelar?',
+                                    'Alguns dados seram perdidos.') == 'Yes':
+                    break
 
         self.window.close()
 
 
 if __name__ == '__main__':
-    senha_view = SenhaView(opcao='ALTERAR')
+    senha_view = SenhaView(opcao='NOVO')
 
     senha_view.start()
